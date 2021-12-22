09088d13b3406f7ace17ba2a9c3d7d08adc68271
shaan-alam <shaanalam369@gmail.com>
2021-12-22 18:04:44 +0530

    created a codesnippet component

---
+++ components/CodeSnippet.js
@@ -0,0 +1,31 @@
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/oceanicNext'
+
+
+const CodeSnippet = ({ children, language }) => {
+
+  return (
+    <>
+      <Highlight
+        {...defaultProps}
+        code={children()}
+        language={language}
+        theme={theme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </>
+  )
+}
+
+export default CodeSnippet
