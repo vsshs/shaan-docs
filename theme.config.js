c5175a8c0899602b643d18d79d6b0593b75c60e3
shaan-alam <shaanalam369@gmail.com>
2021-12-24 10:45:10 +0530

    changed favicon

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/shaan-alam',
   docsRepositoryBase: 'https://github.com/shaan-alam/shaan-docs',
-  titleSuffix: ' 👨‍💻',
+  titleSuffix: '',
   logo: (
     <>
       <span className="mr-2 font-extrabold logo dark">
